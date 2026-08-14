2e28628d936079b6dd60b039cad0bfce0724310f
bittokazi <bitto.kazi@gmail.com>
2026-08-14 13:17:43 +0200

    fix: extra claims :bug:

--- backend/src/main/kotlin/ktor/oauth2/multi/tenant/auth/server/security/config/ExtraTokenClaimConfig.kt
+++ backend/src/main/kotlin/ktor/oauth2/multi/tenant/auth/server/security/config/ExtraTokenClaimConfig.kt
@@ -1,11 +1,14 @@
 package ktor.oauth2.multi.tenant.auth.server.security.config
 
 import io.ktor.server.plugins.di.annotations.Property
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class ExtraTokenClaimConfig(
     @Property("extra-token-claims") val extraTokenClaims: List<ExtraTokenClaim> = listOf(),
 )
 
+@Serializable
 data class ExtraTokenClaim(
     val clientId: String,
     val claimName: String,
@@ -13,6 +16,7 @@ data class ExtraTokenClaim(
     val claimValue: String,
 )
 
+@Serializable
 enum class ExtraTokenClaimType {
     STRING,
     LIST,
