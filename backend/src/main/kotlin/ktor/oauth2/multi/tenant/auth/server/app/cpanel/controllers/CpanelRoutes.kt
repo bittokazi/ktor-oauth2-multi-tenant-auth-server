14487198ccf53046ab4565f572b0b4fc06faa630
bittokazi <bitto.kazi@gmail.com>
2026-09-01 07:58:11 +0200

    bugfix: fixed cpanel template factory 🐛

--- backend/src/main/kotlin/ktor/oauth2/multi/tenant/auth/server/app/cpanel/controllers/CpanelRoutes.kt
+++ backend/src/main/kotlin/ktor/oauth2/multi/tenant/auth/server/app/cpanel/controllers/CpanelRoutes.kt
@@ -2,16 +2,21 @@ package ktor.oauth2.multi.tenant.auth.server.app.cpanel.controllers
 
 import com.bittokazi.ktor.auth.services.TemplateCustomizerFactory
 import com.bittokazi.ktor.auth.utils.respondMustache
+import com.github.mustachejava.DefaultMustacheFactory
+import io.ktor.http.ContentType
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.http.content.staticFiles
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.plugins.di.dependencies
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import ktor.oauth2.multi.tenant.auth.server.app.config.AppModuleConfiguration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.io.StringWriter
 
 fun Application.cpanelRoutes() {
     val log: Logger = LoggerFactory.getLogger(javaClass)
@@ -26,16 +31,24 @@ fun Application.cpanelRoutes() {
 
     routing {
         get("/app") {
-            call.respondMustache(
-                templateCustomizerFactory,
+            respondCpanelTemplate(
+                call,
                 "cpanel.hbs",
                 mapOf<String, Any>(),
             )
         }
 
         get("/app/") {
-            call.respondMustache(
-                templateCustomizerFactory,
+            respondCpanelTemplate(
+                call,
+                "cpanel.hbs",
+                mapOf<String, Any>(),
+            )
+        }
+
+        get("/app/{...}") {
+            respondCpanelTemplate(
+                call,
                 "cpanel.hbs",
                 mapOf<String, Any>(),
             )
@@ -51,4 +64,21 @@ fun Application.cpanelRoutes() {
     }
 
     log.info("[cpanelRoutes] -> Routes configured.")
+}
+
+private suspend fun respondCpanelTemplate(
+    call: ApplicationCall,
+    template: String,
+    model: Any,
+) {
+    val factory = DefaultMustacheFactory("templates")
+
+    val html =
+        StringWriter().also {
+            factory.compile(template)
+                .execute(it, model)
+                .flush()
+        }.toString()
+
+    call.respondText(html, ContentType.Text.Html)
 }
